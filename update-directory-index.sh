e47c3f00ada983bb9f2eff5753462f2c3bf5c845
y-yoshinoya <y2.void+github@gmail.com>
2012-04-10 15:08:38 +0900

    表示ひどかったので修正

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -2,8 +2,8 @@
 
 for DIR in $(find maven -type d); do
   (
-    echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<div>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
-    echo -e "</div>\n</body>\n</html>"
+    echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<ul>"
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<li><a href=\"%s\">%s</a></li>\n",$1,$1 }'
+    echo -e "</ul>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
